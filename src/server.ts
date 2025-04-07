18e5dadf63caf2ed91655e5c643230cde5a2addc
n_masharipov <n.masharipov@uzinfocom.uz>
2025-04-07 10:25:12 +0500

    route update

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,7 @@ import overviewRoutes from "./routes/overview.routes";
 import contactRoutes from "./routes/contact.routes";
 import professorRoutes from "./routes/professor.routes";
 import memberRoutes from "./routes/member.routes";
+import professorImageRoutes from "./routes/professor-image-upload.routes";
 
 const app = express();
 app.use(express.json());
@@ -20,6 +21,7 @@ app.use("/api/overviews", overviewRoutes);
 app.use("/api/contacts", contactRoutes);
 app.use("/api/professors", professorRoutes);
 app.use("/api/members", memberRoutes);
+app.use("/api/professor-image-upload", professorImageRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
