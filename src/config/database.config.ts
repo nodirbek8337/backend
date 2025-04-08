b986f01dab5831981ef42c09a1b2874d03b0b354
n_masharipov <n.masharipov@uzinfocom.uz>
2025-04-08 17:48:11 +0500

    MONGO_URL

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -2,11 +2,11 @@ import mongoose from "mongoose";
 
 export const dbConnect = async (): Promise<void> => {
   try {
-    if (!process.env.MONGO_URI) {
-      throw new Error("MONGO_URI muhit o'zgaruvchisi aniqlanmagan");
+    if (!process.env.MONGO_URL) {
+      throw new Error("MONGO_URL muhit o'zgaruvchisi aniqlanmagan");
     }
 
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URL);
     console.log("MongoDB ga muvaffaqiyatli ulandi!");
   } catch (error) {
     console.error("MongoDB ulanishida xatolik:", error);
