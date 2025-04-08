77f6fe44b160046b9ef402c23ead0b2315c00595
Masharipov_N <127229041+nodirbek8337@users.noreply.github.com>
2025-04-08 16:12:36 +0500

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -25,7 +25,7 @@ app.use("/api/members", memberRoutes);
 app.use("/api/professor-image-upload", professorImageRoutes);
 app.use("/api/member-image-upload", memberImageRoutes);
 
-const PORT = process.env.PORT || 1000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server http://localhost:${PORT} da ishlamoqda`);
 });
